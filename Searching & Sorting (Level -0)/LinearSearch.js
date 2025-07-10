ddcbbd3e283c307ce642befa1d91cb24e88b7f62
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2025-07-10 17:37:01 +0000

    searchingand sorting

---
+++ Searching & Sorting (Level -0)/LinearSearch.js
@@ -0,0 +1,15 @@
+const arr = [2,4,5,6,8,9] 
+const target = 21;
+
+
+const index = function linearSearch(arr,target){
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] == target) return i;
+    }
+
+    return -1
+}
+
+
+console.log(index(arr,target))
